1c0185c0d188b505ed2ca9f4e94588b34d832c53
Sheba <shebab971@gmail.com>
2025-07-16 19:07:48 +0530

    Add files via upload

---
+++ Password Generator/script.js
@@ -0,0 +1,45 @@
+let generateBtn=document.getElementById('generate-btn');
+let copyBtn=document.getElementById('copy-btn');
+let generatedPassword=document.getElementById('generated-password');
+
+generateBtn.onclick=()=>{
+    let length=document.getElementById('length').value;
+    let lowercase=document.getElementById('lowercase').checked;
+    let uppercase=document.getElementById('uppercase').checked;
+    let numbers=document.getElementById('numbers').checked;
+    let symbols=document.getElementById('symbols').checked;
+
+    let charSet='';
+    if(lowercase){
+        charSet += 'abcdefghijklmnopqrstuvwxyz';
+    }
+    if(uppercase){
+        charSet += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    }
+    if(numbers){
+        charSet += '0123456789';
+    }
+    if(symbols){
+        charSet += '!@#$%^&*()_-+={}[];\':",<.>/?|~`';
+    }
+    if(!charSet){
+        alert("Please select at least one character type");
+        return;
+    }
+    let password='';
+    for(var i=0; i<length; i++)
+    {
+        let randomIndex=Math.floor(Math.random()*charSet.length);
+        password+=charSet[randomIndex];
+    }
+    generatedPassword.textContent=password;
+    copyBtn.style.display='block';
+}
+copyBtn.onclick=()=>{
+    let copy=generatedPassword.textContent;
+    navigator.clipboard.writeText(copy).then(()=>{
+        alert("Password copied to clipboard!");
+    }).catch(error =>{
+        alert("Failed to copy password: "+error);
+    });
+};
